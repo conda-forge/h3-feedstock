e47d463e86efb8ff8f8fa2773b611a40b8216958
benhuff <benhuff10@gmail.com>
2019-10-09 12:59:38 -0400

    bld.bat + meta.yaml changes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,17 +3,14 @@ cd build
 
 cmake -G "NMake Makefiles" ^
       -D CMAKE_BUILD_TYPE=Release ^
-      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
-      -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
+      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       ..
 if errorlevel 1 exit 1
 
-cmake --build . --config Release
+cmake --build .
 if errorlevel 1 exit 1
 
-cmake --build . --target install --config Release
+cmake --build . --target install
 if errorlevel 1 exit 1
-
-copy %LIBRARY_LIB%\h3-3.6.0.lib %LIBRARY_LIB%\h3.lib
