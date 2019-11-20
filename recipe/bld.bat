9050c00fcedd1ba5ffa5861e2f716579d3e0ba34
benhuff <benhuff10@gmail.com>
2019-11-20 16:17:33 -0500

    crazy... take out nmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,13 +1,12 @@
 mkdir build-shared
 cd build-shared
 
-cmake -G "NMake Makefiles" ^
-      -D CMAKE_BUILD_TYPE=Release ^
+cmake -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -D BUILD_SHARED_LIBS=TRUE ^
       -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
+      -D BUILD_SHARED_LIBS=ON ^
       ..
 if errorlevel 1 exit 1
 
