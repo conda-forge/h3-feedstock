d5422198bf6eb4a037f3898aea5915b09e71d773
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-11-20 17:18:37 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,7 @@ cd build
 set CMAKE_GENERATOR="NMake Makefiles"
 cmake -G %CMAKE_GENERATOR% ^
       %COMPILER% ^
+      %SRC_DIR% ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=FALSE
       -DBUILD_SHARED_LIBS:BOOL=ON ^
