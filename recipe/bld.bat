da72317e9a0096e298190fb96bb61a1d23a2215e
benhuff <benhuff10@gmail.com>
2019-11-20 14:37:50 -0500

    nmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,19 @@
-mkdir build
-cd build
+mkdir build-shared
+cd build-shared
 
 cmake -G "NMake Makefiles" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -Dh3_WITH_ZLIB=ON ^
       -Dh3_BUILD_SHARED_LIBS=ON ^
+      -Dh3_MSVC_STATIC_RUNTIME=OFF ^
       ..
 if errorlevel 1 exit 1
 
-cmake --build .
+nmake
 if errorlevel 1 exit 1
 
-cmake --build . --target install
+nmake check
 if errorlevel 1 exit 1
