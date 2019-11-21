75adf05cc22d380266d723e1e7bc9017c387907e
benhuff <benhuff10@gmail.com>
2019-11-21 09:37:14 -0500

    include

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,10 +8,9 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
+      -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
       -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=FALSE ^
       -D BUILD_SHARED_LIBS=ON ^
-      -D ENABLE_FORMAT=OFF ^
-      %SRC_DIR%
       ..
 if errorlevel 1 exit 1
 
