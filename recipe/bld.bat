298514a1a142e429eacfc6adee3d324d598ef55f
benhuff <benhuff10@gmail.com>
2019-11-21 10:16:26 -0500

    back to basic .bat script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,7 +9,8 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
-      ..
+      -D BUILD_SHARED_LIBS=ON ^
+      .
 if errorlevel 1 exit 1
 
 cmake --build . --config Release
