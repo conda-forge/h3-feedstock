f05ed5bb729ebc0ed14d9f14da084cff99b1d11d
benhuff <benhuff10@gmail.com>
2019-11-21 09:59:13 -0500

    back to basic .bat script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,14 +3,12 @@ cd build
 
 set VERBOSE=1
 
-cmake -G "Visual Studio 14 2015 Win64" ^
+cmake -G "NMake Makefiles" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
-      -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=FALSE ^
-      -D BUILD_SHARED=ON ^
       ..
 if errorlevel 1 exit 1
 
