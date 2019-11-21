3383568750b057e699a9b2057361191652480743
benhuff <benhuff10@gmail.com>
2019-11-20 22:09:05 -0500

    mimick install.sh

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,18 +3,19 @@ cd build
 
 set VERBOSE=1
 
-cmake -G "NMake Makefiles" ^
+cmake -G "Visual Studio 14 Win64" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=FALSE ^
-      -D BUILD_SHARED_LIBS:BOOL=ON ^
+      -D BUILD_SHARED_LIBS=ON ^
+      -D ENABLE_FORMAT=OFF ^
       ..
 if errorlevel 1 exit 1
 
 cmake --build .
 if errorlevel 1 exit 1
 
-cmake --build . --target ALL_BUILD
+cmake --build . --target h3
 if errorlevel 1 exit 1
