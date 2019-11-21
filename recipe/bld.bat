1f060697ccffdb007ef79a7735eb28e6b7e4f463
benhuff <benhuff10@gmail.com>
2019-11-20 21:37:21 -0500

    verbose messages

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,8 @@
 mkdir build
 cd build
 
+set VERBOSE=1
+
 cmake -G "NMake Makefiles" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
@@ -8,7 +10,6 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=FALSE ^
       -D BUILD_SHARED_LIBS:BOOL=ON ^
-      -D MSVC_STATIC_RUNTIME=OFF ^
       ..
 if errorlevel 1 exit 1
 
