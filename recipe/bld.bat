c4080d3074cc67095846c8e3f260bcbb13eba4dd
benhuff <benhuff10@gmail.com>
2019-11-21 09:51:58 -0500

    vs2015 win64 as cmake generator

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,14 +3,14 @@ cd build
 
 set VERBOSE=1
 
-cmake -G "NMake Makefiles" ^
+cmake -G "Visual Studio 14 2015 Win64" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
       -D CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=FALSE ^
-      -D BUILD_SHARED_LIBS=ON ^
+      -D BUILD_SHARED=ON ^
       ..
 if errorlevel 1 exit 1
 
