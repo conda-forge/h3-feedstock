73a1c876beb445557fe728a92e2331e8539ea740
benhuff <benhuff10@gmail.com>
2019-11-21 10:37:14 -0500

    back to basic .bat script

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,13 +3,12 @@ cd build
 
 set VERBOSE=1
 
-cmake -G "NMake Makefiles" ^
-      -D CMAKE_BUILD_TYPE=Release ^
-      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
-      -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
+cmake ^
+      -D ENABLE_FORMAT=OFF ^
       -D BUILD_SHARED_LIBS=ON ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -DCMAKE_SKIP_INSTALL_ALL_DEPENDENCY=ON ^
       .
 if errorlevel 1 exit 1
 
