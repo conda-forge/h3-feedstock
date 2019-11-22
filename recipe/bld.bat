e63417296cb5f772ac73147436d93f2404aa2724
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-11-22 13:03:25 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,12 +5,14 @@ cd %BUILD_DIR%
 
 echo "%SRC_DIR%"
 
-set CMAKE_GENERATOR="NMake Makefiles"
+#set CMAKE_GENERATOR="NMake Makefiles"
+set CMAKE_GENERATOR="Visual Studio 15 2017 Win64" ..
 cmake -G %CMAKE_GENERATOR% ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
       -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -D BUILD_SHARED_LIBS:BOOL=ON ^
       ..
 if errorlevel 1 exit 1
 
@@ -20,21 +22,5 @@ if errorlevel 1 exit 1
 cmake --build . --target install
 if errorlevel 1 exit 1
 
-ls -LR
-
-cmake --build . --target all_build
-if errorlevel 1 exit 1
-
-@if not defined _echo echo off
-for /f "usebackq delims=" %%i in (`vswhere.exe -prerelease -latest -property installationPath`) do (
-  if exist "%%i\Common7\Tools\vsdevcmd.bat" (
-    %comspec% /k "%%i\Common7\Tools\vsdevcmd.bat" %*
-    exit /b
-  )
-)
-
-rem Instance or command prompt not found
-exit /b 2
-
-msbuild ALL_BUILD.vcxproj
-msbuild RUN_TESTS.vcxproj
+#msbuild ALL_BUILD.vcxproj
+#msbuild RUN_TESTS.vcxproj
