9fec9d9c9ff1637da275ec5d2eee0a21537ada6c
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-11-22 10:58:33 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -20,12 +20,21 @@ if errorlevel 1 exit 1
 cmake --build . --target install
 if errorlevel 1 exit 1
 
-ls 
-cd generated
-ls
+ls -LR
 
 cmake --build . --target all_build
 if errorlevel 1 exit 1
+
+@if not defined _echo echo off
+for /f "usebackq delims=" %%i in (`vswhere.exe -prerelease -latest -property installationPath`) do (
+  if exist "%%i\Common7\Tools\vsdevcmd.bat" (
+    %comspec% /k "%%i\Common7\Tools\vsdevcmd.bat" %*
+    exit /b
+  )
+)
+
+rem Instance or command prompt not found
+exit /b 2
 
 msbuild ALL_BUILD.vcxproj
 msbuild RUN_TESTS.vcxproj
