2de9684a4f7b78dece0e33d567bd8b75342490ea
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-11-22 14:54:10 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,8 +6,8 @@ cmake -G %CMAKE_GENERATOR% ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
-      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      ..
+      -D BUILD_SHARED_LIBS:BOOL=ON ^
+      %SRC_DIR%
 if errorlevel 1 exit 1
 
 cmake --build .
