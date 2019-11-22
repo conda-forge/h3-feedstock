5312576ab2ef550cd5dd41ed4f0f018db4483faa
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-11-22 15:58:09 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,10 +1,11 @@
 mkdir build
 cd build
 
+set VERBOSE=1
 set VCINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\"
 set VS140COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\"
-
 set CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
+
 cmake -G %CMAKE_GENERATOR% ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
