3bd6bda10d90d3fe6f07b88ad57fc72dbb27def3
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-11-25 14:49:47 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,15 +5,15 @@ set VERBOSE=1
 set "CFLAGS= -g"
 
 
-VCIDEInstallDir = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\
-VCINSTALLDIR = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\
-VCTargetsPath = C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\VCTargets\
-VCTargetsPath10 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\
-VCTargetsPath11 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V110\
-VCTargetsPath12 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120\
-VCTargetsPath14 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140\
-VCToolsInstallDir = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\
-VCToolsRedistDir = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\Redist\
+set VCIDEInstallDir = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\
+set VCINSTALLDIR = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\
+set VCTargetsPath = C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\VCTargets\
+set VCTargetsPath10 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\
+set VCTargetsPath11 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V110\
+set VCTargetsPath12 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120\
+set VCTargetsPath14 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140\
+set VCToolsInstallDir = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\
+set VCToolsRedistDir = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\Redist\
 
 set CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
 
