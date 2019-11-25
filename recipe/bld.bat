2fd9f92670464aa137306c9acd0f4693ce385e92
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-11-25 14:16:40 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,16 +5,8 @@ cd build
 
 set VERBOSE=1
 set "CFLAGS= -g"
-set VCIDEInstallDir=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\VC\
-set VS150COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\
-set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\VC\
-set VCTargetsPath=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\VCTargets\
-set VCTargetsPath15=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\VCTargets\
-set VCToolsInstallDir=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Tools\MSVC\14.16.27012\
-set VCToolsRedistDir=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Redist\MSVC\14.16.27012\
 
-set CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
-
+set CMAKE_GENERATOR="Visual Studio 14 2015 Win64"
 cmake -G %CMAKE_GENERATOR% ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
