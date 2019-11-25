6ecd90d5756cdc69ce86bf0a86fac2ddcead678e
Andrew Copenhaver <andrew.copenhaver@maxar.com>
2019-11-25 14:08:00 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,17 +1,17 @@
 mkdir build
 cd build
 
+14.16.27012
+
 set VERBOSE=1
 set "CFLAGS= -g"
-VCIDEInstallDir = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\
-VCINSTALLDIR = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\
-VCTargetsPath = C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\IDE\VC\VCTargets\
-VCTargetsPath10 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\
-VCTargetsPath11 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V110\
-VCTargetsPath12 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120\
-VCTargetsPath14 = C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140\
-VCToolsInstallDir = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\
-VCToolsRedistDir = C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\Redist\
+set VCIDEInstallDir=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\VC\
+set VS150COMNTOOLS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\
+set VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\VC\
+set VCTargetsPath=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\VCTargets\
+set VCTargetsPath15=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VC\VCTargets\
+set VCToolsInstallDir=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Tools\MSVC\14.16.27012\
+set VCToolsRedistDir=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Redist\MSVC\14.16.27012\
 
 set CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
 
