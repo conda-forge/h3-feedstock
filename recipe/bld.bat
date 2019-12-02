a9a6a22f560cf8a2b578c6843b44a1c813e23d32
benhuff <benhuff10@gmail.com>
2019-12-02 09:25:49 -0500

    build dynamic libraries for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,21 @@
 mkdir build
 cd build
 
-cmake -G "NMake Makefiles" ^
+set "CFLAGS= /LD"
+set "CXXFLAGS= /LD"
+
+set CMAKE_GENERATOR="NMake Makefiles"
+cmake -G %CMAKE_GENERATOR% ^
       -D CMAKE_BUILD_TYPE=Release ^
+      -D BUILD_SHARED_LIBS=TRUE ^
+      -D BUILD_GENERATORS=FALSE ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
-      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       ..
 if errorlevel 1 exit 1
 
-cmake --build .
+cmake --build . --config Release
 if errorlevel 1 exit 1
 
-cmake --build . --target install
+cmake --build . --config Release --target install
 if errorlevel 1 exit 1
