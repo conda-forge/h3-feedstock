9996a3bc3ee599ee8a22647ec9f1bba87ef9dd22
rluria14 <ryanlur14@gmail.com>
2021-05-03 12:11:33 -0400

    -D BUILD_TESTING=OFF

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,5 +4,20 @@ cd build
 set "CFLAGS= -LD"
 set "CXXFLAGS= -LD"
 
-cmake ..
+cmake -G "NMake Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D BUILD_SHARED_LIBS=TRUE ^
+      -D BUILD_GENERATORS=TRUE ^
+      -D BUILD_TESTING=OFF ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      ..
 if errorlevel 1 exit 1
+
+cmake --build .
+if errorlevel 1 exit 1
+
+cmake --build . --target install
+if errorlevel 1 exit 1
+
+:: Move the .dll file to the Library\lib folder.
+move %LIBRARY_BIN%\h3.dll %LIBRARY_LIB%\h3.dll
