0d92cc815a99e6de74c019592c7f92616c16a768
rluria14 <ryanlur14@gmail.com>
2021-05-03 13:28:33 -0400

    bld testing off

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,6 +8,7 @@ cmake -G "NMake Makefiles" ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D BUILD_SHARED_LIBS=TRUE ^
       -D BUILD_GENERATORS=TRUE ^
+      -D BUILD_TESTING=OFF ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       ..
 if errorlevel 1 exit 1
