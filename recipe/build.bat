38ad072306ddaa3cf94c86e7eef951b88006db9b
Lindsey Gray <lindsey.gray@gmail.com>
2025-07-01 01:59:41 -0500

    fix: moving the .dll from bin to lib breaks cmake (#35)
    * fix: moving the .dll from bin to lib breaks cmake
    
    * increment build number
    
    * move to package_contents test
    
    * add additional supported build platforms
    
    * use pixi
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.1, and conda-forge-pinning 2025.06.30.21.25.52
    
    * MNT: Re-rendered with conda-build 25.5.0, conda-smithy 3.50.1, and conda-forge-pinning 2025.06.30.21.25.52
    
    * use windows CMAKE_ARGS as well
    
    * change include check to h3/h3api.h
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.bat
+++ recipe/build.bat
@@ -5,6 +5,7 @@ set "CFLAGS= -LD"
 set "CXXFLAGS= -LD"
 
 cmake -G "NMake Makefiles" ^
+      %CMAKE_ARGS% ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D BUILD_SHARED_LIBS=TRUE ^
       -D BUILD_GENERATORS=FALSE ^
@@ -24,6 +25,3 @@ if errorlevel 1 exit 1
 
 cmake --build . --target install
 if errorlevel 1 exit 1
-
-:: Move the .dll file to the Library\lib folder.
-move %LIBRARY_BIN%\h3.dll %LIBRARY_LIB%\h3.dll
